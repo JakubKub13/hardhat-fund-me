fe202427a11a95e752ffc8cff8e23073684af21f
JK1331 <kubala.jakub13@gmail.com>
2022-08-09 08:53:46 +0200

    initial commit

---
+++ scripts/fund.js
@@ -0,0 +1,20 @@
+const { getNamedAccounts, ethers } = require("hardhat")
+
+
+async function main() {
+    const { deployer } = await getNamedAccounts()
+    const fundMe = await ethers.getContract("FundMe", deployer)
+    console.log("Funding Contract....")
+    const transactionResponse = await fundMe.fund({value: ethers.utils.parseEther("0.1")})
+    await transactionResponse.wait(1)
+    console.log("Funded!")
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
+
+// to run script npx hardhat node and then npx hardhat run scripts/fund.js --network localhost
